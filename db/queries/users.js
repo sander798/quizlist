621658de4b5bd35e4f02770d0247773544816bbe
hktaneja <hktaneja2020@gmail.com>
2023-09-18 19:09:54 -0500

    add database queries

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,10 +1,52 @@
 const db = require('../connection');
 
-const getUsers = () => {
-  return db.query('SELECT * FROM users;')
-    .then(data => {
+// Get all public quizzes.
+const getQuizzes = () => {
+  return db.query("SELECT QuizID, Title, Description FROM Quizzes WHERE Visibility = 'public';")
+      .then(data => {
       return data.rows;
     });
 };
 
-module.exports = { getUsers };
+// Open the quiz.
+const getQuiz = (quizId) => {
+  return db.query('SELECT Title, Description FROM Quizzes WHERE QuizID = $1', [quizId])
+      .then(data => {
+      return data.rows;
+    });
+};
+
+
+// Add a new quiz to the database.
+
+const addQuiz = function(quiz) {
+  return pool.query(
+    'INSERT INTO Quizzes (Title, Description, Visibility, CreatorUserID) VALUES ($1, $2, $3,$4) RETURNING *',
+    [quiz.title, quiz.description, quiz.visibility, quiz.userId]
+  )
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return null;
+    });
+};
+
+// Add a new user to the database.
+
+const addUser = function(user) {
+  return pool.query(
+    'INSERT INTO users (name, password, email ) VALUES ($1, $2, $3) RETURNING *',
+    [user.name, user.password, user.email]
+  )
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return null;
+    });
+};
+
+module.exports = { getQuizzes, addQuiz, addUser, getQuiz };
--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,54 @@
--- Drop and recreate Users table (Example)
+-- Drop and recreate tables 
 
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS quizzes CASCADE;
+DROP TABLE IF EXISTS questions CASCADE;
+DROP TABLE IF EXISTS AnswerIDnswers CASCADE;
+DROP TABLE IF EXISTS results CASCADE;
+DROP TABLE IF EXISTS result_questions CASCADE;
+
+
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL    
+);
+
+CREATE TABLE quizzes (
+    id SERIAL PRIMARY KEY NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    is_public BOOLEAN NOT NULL,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE questions (
+    id SERIAL PRIMARY KEY NOT NULL,
+    text TEXT NOT NULL,
+    quiz_id INTEGER REFERENCES quizzes(id) ON DELETE CASCADE
+);
+
+CREATE TABLE answers (
+    id SERIAL PRIMARY KEY NOT NULL,
+    text TEXT NOT NULL,
+    is_correct BOOLEAN NOT NULL,
+    question_id INTEGER REFERENCES questions(id) ON DELETE CASCADE
+);
+
+CREATE TABLE results (
+    id SERIAL PRIMARY KEY NOT NULL,
+    quiz_id INTEGER REFERENCES quizzes(id) ON DELETE CASCADE,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    date DATE,
+    score INTEGER
+);
+    
+
+CREATE TABLE result_questions (
+    id SERIAL PRIMARY KEY NOT NULL,
+    result_id INTEGER REFERENCES results(id) ON DELETE CASCADE,
+    question_id INTEGER REFERENCES questions(id) ON DELETE CASCADE,
+    selected_answer_id INTEGER REFERENCES answers(id) ON DELETE CASCADE,
+    user_response TEXT 
 );
