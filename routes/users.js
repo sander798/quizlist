ee3b04b48f30facf3cfb0372d9966ca7bfdcc636
Taylor Maine <123534861+moogleKupo@users.noreply.github.com>
2023-09-21 16:04:53 -0600

    Update naming convention

--- routes/users.js
+++ routes/users.js
@@ -11,13 +11,14 @@ const { getUserById } = require('../db/queries/users');
 
 // Separated Routes
 const quizRoutes = require('./users_quiz');
-const accountRoute = require('./users_account');
-const attemptRoute = require('./users_attempt');
+const accountRoutes = require('./users_account');
+const attemptRoutes = require('./users_attempt');
 const loginRoutes = require('./users_login');
 
 // Mount all resource routes
 router.use('/quiz', quizRoutes);
-router.use('/attempt', attemptRoute);
+router.use('/account', accountRoutes);
+router.use('/attempt', attemptRoutes);
 router.use('/login', loginRoutes);
 
 // Home page
