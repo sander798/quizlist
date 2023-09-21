792b5994f3343cb7b8e3e8b24944932f8113c0fc
Taylor Maine <123534861+moogleKupo@users.noreply.github.com>
2023-09-21 16:17:01 -0600

    Create users_quiz.js

---
+++ routes/users_quiz.js
@@ -0,0 +1,14 @@
+// New quiz creation page
+router.get('/new', (req, res) => {
+  const userId = req.session.userId;
+
+  if (!userId) {
+    return res.redirect('/quizapp/login');
+  }
+
+  // Retrieve user information by user ID
+  getUserById(userId).then((user) => {
+    const templateVars = { userName: user.name };
+    res.render('quiz_form', templateVars);
+  });
+});
