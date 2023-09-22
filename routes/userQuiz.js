7c7f2c332949a8008487f565cebdc1ac67f6a66a
Taylor Maine <123534861+moogleKupo@users.noreply.github.com>
2023-09-22 13:50:07 -0600

    Add DataHelpers export module

--- routes/userQuiz.js
+++ routes/userQuiz.js
@@ -2,53 +2,55 @@ const express = require('express');
 const router = express.Router();
 const { getUserById, getQuizResult, getQuizDetails, getQuiz } = require('../db/queries/users'); // Updated imports
 
-// Create new page
-router.get('/new', (req, res) => {
-  const userId = req.session.userId;
+module.exports = function(DataHelpers) {
+  // Create new quiz page
+  router.get('/new', (req, res) => {
+    const userId = req.session.userId;
 
-  if (!userId) {
-    return res.redirect('/quizapp/login');
-  }
+    if (!userId) {
+      return res.redirect('/quizapp/login');
+    }
 
-  getUserById(userId).then((user) => {
-    const templateVars = { userName: user.name };
-    res.render('quiz_form', templateVars);
+    getUserById(userId).then((user) => {
+      const templateVars = { userName: user.name };
+      res.render('quiz_form', templateVars);
+    });
   });
-});
 
-// Page for results of all quizzes taken
-router.get('/results/:url', (req, res) => {
-  const userId = req.session.userId;
-  const templateVars = {};
-  const url = req.params.url;
+  // Page for results of all quizzes taken
+  router.get('/results/:url', (req, res) => {
+    const userId = req.session.userId;
+    const templateVars = {};
+    const url = req.params.url;
 
-  templateVars.url = url;
+    templateVars.url = url;
 
-  Promise.all([getUserById(userId), getQuizResult(url, userId)]) // Updated function name
-    .then(([user, results]) => {
-      templateVars.userName = !user ? '' : user.name;
-      templateVars.results = results;
-      return results[0].quiz_id;
-    })
-    .then((quizId) => getQuizDetails(quizId))
-    .then((quiz) => {
-      templateVars.quiz = quiz;
-      res.render('quiz_stats', templateVars);
-    });
-});
+    Promise.all([getUserById(userId), getQuizResult(url, userId)]) // Updated function name
+      .then(([user, results]) => {
+        templateVars.userName = !user ? '' : user.name;
+        templateVars.results = results;
+        return results[0].quiz_id;
+      })
+      .then((quizId) => getQuizDetails(quizId))
+      .then((quiz) => {
+        templateVars.quiz = quiz;
+        res.render('quiz_stats', templateVars);
+      });
+  });
 
-// Page to take a quiz
-router.get('/:url', (req, res) => {
-  const userId = req.session.userId;
+  // Page to take a quiz
+  router.get('/:url', (req, res) => {
+    const userId = req.session.userId;
 
-  Promise.all([getUserById(userId), getQuiz({ url: req.params.url })])
-    .then(([user, quiz]) => {
-      const templateVars = {
-        userName: !user ? '' : user.name,
-        quiz,
-      };
-      res.render('quiz', templateVars);
-    });
-});
+    Promise.all([getUserById(userId), getQuiz({ url: req.params.url })])
+      .then(([user, quiz]) => {
+        const templateVars = {
+          userName: !user ? '' : user.name,
+          quiz,
+        };
+        res.render('quiz', templateVars);
+      });
+  });
 
-module.exports = router;
+  return router;
+};
