4fa72d0b4e56b9d6af758338eda570b52a0a255e
Taylor Maine <123534861+moogleKupo@users.noreply.github.com>
2023-09-22 13:37:29 -0600

    wrap exports in DataHelper function

--- routes/users_quiz.js
+++ routes/users_quiz.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-// New quiz creation page
-router.get('/new', (req, res) => {
-  const userId = req.session.userId;
+module.exports = function (DataHelpers) {
+  // New quiz creation page
+  router.get('/new', (req, res) => {
+    const userId = req.session.userId;
 
-  if (!userId) {
-    return res.redirect('/quizapp/login');
-  }
+    if (!userId) {
+      return res.redirect('/quizapp/login');
+    }
 
-  // Retrieve user information by user ID
-  getUserById(userId).then((user) => {
-    const templateVars = { userName: user.name };
-    res.render('quiz_form', templateVars);
+    // Use the getUserById function from DataHelpers
+    DataHelpers.getUserById(userId).then((user) => {
+      const templateVars = { userName: user.name };
+      res.render('quiz_form', templateVars);
+    });
   });
-});
+
+  return router;
+};
