0ed2fad2bbd7b455f2734f3f827f0cc29ab4ecea
Taylor Maine <123534861+moogleKupo@users.noreply.github.com>
2023-09-22 15:40:02 -0600

    Add eventListener for buttons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,9 @@
 // Client facing scripts here
+<script>
+  document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('myButton');
+    button.addEventListener('click', () => {
+      alert('Button Clicked!');
+    });
+  });
+</script>
