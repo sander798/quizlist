0b8362b48bd88d43fa86cdfc84516699a7e14589
Taylor Maine <123534861+moogleKupo@users.noreply.github.com>
2023-09-22 13:24:58 -0600

    adjusting export function

--- routes/attempt.js
+++ routes/attempt.js
@@ -1,30 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const { getUserById } = require('../db/queries/users');
-const { getQuizResult, getQuizDetails, getQuiz } = require('../db/queries/users'); // Updated imports
 
-// Single attempt page
-router.get('/:url', (req, res) => {
-  const userId = req.session.userId;
-  const url = req.params.url;
+module.exports = function(DataHelpers) {
+  // Single attempt page
+  router.get('/:url', (req, res) => {
+    const userId = req.session.userId;
+    const url = req.params.url;
 
-  Promise.all([
-    getUserById(userId),
-    getQuizResult(url, userId), // Updated function name
-  ])
-    .then(([user, attempt]) => {
-      const templateVars = {
-        attempt,
-        userName: !user ? '' : user.name,
-      };
-      return attempt[0].quiz_id;
-    })
-    .then((quizId) => getQuizDetails(quizId))
-    .then((quiz) => {
-      templateVars.quiz = quiz;
-      res.render('quiz_attempt', templateVars);
-    })
-    .catch((error) => console.log(error));
-});
+    Promise.all([
+      DataHelpers.getUserById(userId),
+      DataHelpers.getQuizResult(url, userId), // Updated function name
+    ])
+      .then(([user, attempt]) => {
+        const templateVars = {
+          attempt,
+          userName: !user ? '' : user.name,
+        };
+        return attempt[0].quiz_id;
+      })
+      .then((quizId) => DataHelpers.getQuizDetails(quizId))
+      .then((quiz) => {
+        templateVars.quiz = quiz;
+        res.render('quiz_attempt', templateVars);
+      })
+      .catch((error) => console.log(error));
+  });
 
-module.exports = router;
+  return router;
+};
