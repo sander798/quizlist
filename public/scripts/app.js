ca7ea53632feb8880c7506ce4de87bdf3f7632cc
Taylor Maine <123534861+moogleKupo@users.noreply.github.com>
2023-09-22 16:57:08 -0600

    remove register and login

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -15,16 +15,6 @@ document.addEventListener('DOMContentLoaded', () => {
     window.location.href = '/quiz/new';
   });
 
-  // Click event for the "Login" button on the home page (index.ejs)
-  $('#login-button').click(function () {
-    window.location.href = '/login';
-  });
-
-  // Click event for the "Register" button on the login page (login.ejs)
-  $('#register-button').click(function () {
-    window.location.href = '/login/new';
-  });
-
   // Click event for the "Submit Answers" button on the quiz page (quiz.ejs)
   $('#submit-answers-button').click(function () {
     window.location.href = '/results';
