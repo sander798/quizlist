ba07dd808579b2b873b2c25c6d62bd660b3920de
Taylor Maine <123534861+moogleKupo@users.noreply.github.com>
2023-09-22 16:31:26 -0600

    Create eventListener and click events for buttons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,32 @@
-// Client facing scripts here
-<script>
-  document.addEventListener('DOMContentLoaded', () => {
-    const button = document.getElementById('myButton');
-    button.addEventListener('click', () => {
-      alert('Button Clicked!');
-    });
+// Client-facing scripts here
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('myButton');
+  button.addEventListener('click', () => {
+    alert('Button Clicked!');
   });
-</script>
+
+  // Click event for the "Take Quiz" button on the home page (index.ejs)
+  $('#take-quiz-button').click(function () {
+    window.location.href = '/quiz';
+  });
+
+  // Click event for the "Create New Quiz" button on the home page (index.ejs)
+  $('#create-quiz-button').click(function () {
+    window.location.href = '/quiz/new';
+  });
+
+  // Click event for the "Login" button on the home page (index.ejs)
+  $('#login-button').click(function () {
+    window.location.href = '/login';
+  });
+
+  // Click event for the "Register" button on the login page (login.ejs)
+  $('#register-button').click(function () {
+    window.location.href = '/login/new';
+  });
+
+  // Click event for the "Submit Answers" button on the quiz page (quiz.ejs)
+  $('#submit-answers-button').click(function () {
+    window.location.href = '/results';
+  });
+});
