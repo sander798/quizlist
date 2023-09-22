05b54933f4a69a437f2c6a5efbaf1f253a86f5a6
Taylor Maine <123534861+moogleKupo@users.noreply.github.com>
2023-09-22 14:10:06 -0600

    Add templateVars declaration near top

--- routes/attempt.js
+++ routes/attempt.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-module.exports = function(DataHelpers) {
+module.exports = function (DataHelpers) {
   // Single attempt page
   router.get('/:url', (req, res) => {
     const userId = req.session.userId;
     const url = req.params.url;
+    const templateVars = {};
 
     Promise.all([
       DataHelpers.getUserById(userId),
-      DataHelpers.getQuizResult(url, userId), // Updated function name
+      DataHelpers.getQuizResult(url, userId),
     ])
       .then(([user, attempt]) => {
-        const templateVars = {
-          attempt,
-          userName: !user ? '' : user.name,
-        };
+        templateVars.attempt = attempt;
+        templateVars.userName = !user ? '' : user.name;
         return attempt[0].quiz_id;
       })
       .then((quizId) => DataHelpers.getQuizDetails(quizId))
@@ -23,7 +22,11 @@ module.exports = function(DataHelpers) {
         templateVars.quiz = quiz;
         res.render('quiz_attempt', templateVars);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.error(error);
+        // Handle errors here, like sending an error response
+        res.status(500).send('Internal Server Error');
+      });
   });
 
   return router;
