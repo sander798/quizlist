e0ce807812c4ce2486e3de666ed491cb55532faa
Taylor Maine <123534861+moogleKupo@users.noreply.github.com>
2023-09-22 19:18:10 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
-const { getUserById, getQuizResult, getQuizDetails, getQuiz } = require('./db/queries/users'); // Import necessary queries
+const { getUserById, getQuizzes } = require('./db/queries/users'); // Import necessary queries
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -14,9 +14,6 @@ const DataHelpers = require('./routes/attempt');
 
 app.set('view engine', 'ejs');
 
-// Load the logger first so all (static) HTTP requests are logged to STDOUT
-// 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(
@@ -29,29 +26,34 @@ app.use(
 );
 app.use(express.static('public'));
 
-// Separated Routes for each Resource
-// Note: Feel free to replace the example routes below with your own
-const userApiRoutes = require('./routes/users-api');
-const widgetApiRoutes = require('./routes/widgets-api');
-const usersRoutes = require('./routes/users')(DataHelpers); // Call with DataHelpers parameter
+// Separated Routes
+const quizRoutes = require('./users_quiz')(DataHelpers);
+const attemptRoutes = require('./attempt')(DataHelpers);
+const loginRoutes = require('./login')(DataHelpers);
 
-// Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
-// Note: Endpoints that return data (eg. JSON) usually start with `/api`
-app.use('/api/users', userApiRoutes);
-app.use('/api/widgets', widgetApiRoutes);
-app.use('/users', usersRoutes);
-// Note: mount other resources here, using the same pattern above
+app.use('/quiz/new', quizRoutes);
+app.use('/attempt', attemptRoutes);
+app.use('/quiz', loginRoutes);
 
 // Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
-
 app.get('/', (req, res) => {
-  res.render('index');
+  const userId = req.session.userId;
+
+  Promise.all([getUserById(userId), getQuizzes()])
+    .then(([user, quizzes]) => {
+      const templateVars = {
+        userName: !user ? '' : user.name,
+        quizzes,
+      };
+      res.render('quiz', templateVars);
+    })
+    .catch((error) => {
+      console.error(error);
+      // Error Handling
+      res.status(500).send('Internal Server Error');
+    });
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
-
