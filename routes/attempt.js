6ca0d8c3b130c35c675090fd11ef80f4ccabe2b4
Alexander Evans <sander7989@gmail.com>
2023-09-23 03:21:36 -0600

    make /attempt/:id at least load

--- routes/attempt.js
+++ routes/attempt.js
@@ -1,26 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../db/queries/users');
 
 module.exports = function (DataHelpers) {
   // Single attempt page
   router.get('/:url', (req, res) => {
-    const userId = req.session.userId;
+    const userId = 0;
     const url = req.params.url;
     const templateVars = {};
 
     Promise.all([
-      DataHelpers.getUserById(userId),
-      DataHelpers.getQuizResult(url, userId),
+      db.getUserById(userId),
+      db.getQuizResult(url, userId),
     ])
       .then(([user, attempt]) => {
         templateVars.attempt = attempt;
-        templateVars.userName = !user ? '' : user.name;
+        templateVars.userName = !user ? 'John Doe' : user.name;
         return attempt[0].quiz_id;
       })
-      .then((quizId) => DataHelpers.getQuizDetails(quizId))
+      .then((quizId) => db.getQuizDetails(quizId))
       .then((quiz) => {
         templateVars.quiz = quiz;
-        res.render('quiz_attempt', templateVars);
+        res.render('results', templateVars);
       })
       .catch((error) => {
         console.error(error);
--- routes/userQuiz.js
+++ routes/userQuiz.js
@@ -61,6 +61,7 @@ router.get('/:url', (req, res) => {
         userName: "John Doe",
         quiz,
       };
+      console.log(templateVars.quiz);
       res.render('quiz', templateVars);
     })
     .catch((error) => {
